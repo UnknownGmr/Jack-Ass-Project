74d370e7ccbca566950bed9ebb9555ff41517246
UnknownGmr <108364983+UnknownGmr@users.noreply.github.com>
2022-06-30 01:49:29 -0500

    Finally its out

---
+++ JackAssMAIN.bat
@@ -0,0 +1,56 @@
+@echo off
+title JackAss 1.0
+chcp 65001
+:A
+echo enter password to activate program.
+set/p "pass=>"
+if NOT %pass%== jackass goto :FAIL
+cls
+start read.md
+
+
+echo.
+echo      ██╗ █████╗  ██████╗██╗  ██╗     █████╗ ███████╗███████╗     ██╗    ██████╗ 
+echo      ██║██╔══██╗██╔════╝██║ ██╔╝    ██╔══██╗██╔════╝██╔════╝    ███║   ██╔═████╗
+echo      ██║███████║██║     █████╔╝     ███████║███████╗███████╗    ╚██║   ██║██╔██║
+echo ██   ██║██╔══██║██║     ██╔═██╗     ██╔══██║╚════██║╚════██║     ██║   ████╔╝██║
+echo ╚█████╔╝██║  ██║╚██████╗██║  ██╗    ██║  ██║███████║███████║     ██║██╗╚██████╔╝
+echo  ╚════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝     ╚═╝╚═╝ ╚═════╝ 
+  echo.
+
+echo ##########################################
+echo # Thanks For Downloading JackAss JackAss #
+echo ##########################################
+echo #       type Lock for private folder     #
+echo #       type ping for pinger             #
+echo #       type ndip For NDIP               #
+echo #      more features coming soon         #
+echo ##########################################
+set /p a=ENTER COMMAND:
+
+if %a%==Lock (
+    goto File
+)
+
+if %a%==ping (
+    goto ping
+)
+
+if %a%==ndip (
+    goto ndip
+)
+
+:File
+start JackAss-Locker.bat
+exit
+
+:ping
+start JackAss-Pinger.bat
+exit
+
+:ndip
+start NDIP(For-JackAss).bat
+exit 
+
+cls
+exit
---
+++ read.md
@@ -0,0 +1,4 @@
+This Tool Is Most Likley only Going To Be Seen And Downloaded By Members of the program dream so you nay know me as Jupiter's Father this tool was made by me and
+is open source so go crazy making your own versions
+i may check some out and heres a tutorial on how to use my Multi Tool 
+PASS FOR EVERYTHING IS jackass u can change this in the source code.
